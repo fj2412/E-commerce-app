7c03dadd9e87408ba42399564fdf862af9c4bb03
fengj <fengjiang1009@gmail.com>
2023-08-20 16:23:06 -0400

    feat: page setup with navbar and product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <h1>Ecommerce app</h1>
-  )
-}
+    <>
+      <Header/>
+        <main className="py-3">
+          <Container>
+            <HomeScreen/>
+          </Container>
+        </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
