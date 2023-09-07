ee496e45e7dac39be03d5e8ec3e256d1216714d2
fengj <fengjiang1009@gmail.com>
2023-09-07 17:58:13 -0400

    feat: user profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,8 @@ import PrivateRoute from "./components/PrivateRoute";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
 
 const App = () => {
   return (
@@ -41,6 +43,8 @@ const App = () => {
                 <Route path='/payment' element={<PaymentScreen/>}/>
                 <Route path='/placeorder' element={<PlaceOrderScreen/>}/>
                 <Route path='/order/:id' element={<OrderScreen/>}/>
+                <Route path='/profile' element={<ProfileScreen/>}/>
+
               </Route>
             </Routes>
           </Container>
