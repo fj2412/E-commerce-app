5408222ea957f2c80fe83b8b0c9081864a5950a6
fengj <fengjiang1009@gmail.com>
2023-09-21 18:38:48 -0400

    feat: search box component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,11 +6,12 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
+import { Link } from "react-router-dom";
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { keyword, pageNumber } = useParams();
 
-  const {data, isLoading, error} = useGetProductsQuery({pageNumber});
+  const {data, isLoading, error} = useGetProductsQuery({keyword, pageNumber});
 
   // const [products, setProducts] = useState([]);
 
@@ -25,6 +26,7 @@ const HomeScreen = () => {
 
   return (
     <>
+      {keyword && (<Link to='/' className='btn btn-light mb-4'>Go Back</Link>)}
       { isLoading ? (<Loader></Loader>) : error ? (<Message variant='danger'>{error?.data?.message 
         || error.error}
       </Message>) : (<>
@@ -36,7 +38,7 @@ const HomeScreen = () => {
             </Col>
           ))}
         </Row>
-        <Paginate pages={data.pages} page={data.page}/>
+        <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''}/>
       </>)}
 
     </>
